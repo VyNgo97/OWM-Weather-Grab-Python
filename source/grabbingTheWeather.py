b8be69d73c26465d6ad64abeeddec187d367e78e
VyNgo97 <42617291+VyNgo97@users.noreply.github.com>
2019-02-12 17:04:37 -0600

    update
    handling for keyerror exception

--- source/grabbingTheWeather.py
+++ source/grabbingTheWeather.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Grabbing the Weather Project for Dwolla Internship
+# By: Vy Ngo
 
-# In[26]:
+# module allows http requests
+import requests
 
-
-#Grabbing the Weather Project for Dwolla Internship
-#By: Vy Ngo
-
-
-# In[49]:
-
-
-#module allows http requests
-import requests 
 
 def weatherGrab():
-    #user inputs desired city
-    city = input('Where are you? ')
+    try:
+        # user inputs desired city
+        city = input('Where are you? ')
 
-    #Open weather map api call w/ key and specified city and units
-    weather_API = ('http://api.openweathermap.org/data/2.5/weather?appid=3529ca8740c75206b4c62134c4344442&q=' + city +'&units=imperial')
+        # Open weather map api call w/ key and specified city and units
+        weather_API = (
+                    'http://api.openweathermap.org/data/2.5/weather?appid=3529ca8740c75206b4c62134c4344442&q=' + city + '&units=imperial')
 
-    #accepts data from request in json format
-    data = requests.get(weather_API).json()
+        # accepts data from request in json format
+        data = requests.get(weather_API).json()
 
-    #specifies data location in json format
-    weather_data = data['main']['temp']
+        # specifies data location in json format
+        weather_data = data['main']['temp']
 
-    #outputs temperature in imperial units
-    print(city + ' Weather: \n' + str(weather_data))
-    
+        # outputs temperature in imperial units
+        print(city + ' Weather: \n' + str(weather_data))
+    except KeyError as e:
+        print("That is an invalid city name.")
+
+
 weatherGrab()
-
-
-# 
